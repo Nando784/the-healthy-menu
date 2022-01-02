822783e6666c764911c62c7dde40b663313cf8d8
Nando <nandosorbello@live.it>
2022-01-02 13:52:21 +0100

    Fix Git Issue

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Dietary from "./pages/Dietary";
+import Intolerance from "./pages/Intolerance";
+import Country from "./pages/Country";
+import History from "./pages/History";
+import Recipe from "./pages/Recipe";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/dietary" element={<Dietary />} />
+          <Route exact path="/intolerance" element={<Intolerance />} />
+          <Route exact path="/country" element={<Country />} />
+          <Route exact path="/history" element={<History />} />
+          <Route exact path="/recipe/:name/:id" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,26 +2,11 @@ import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Dietary from "./pages/Dietary";
-import Intolerance from "./pages/Intolerance";
-import Country from "./pages/Country";
-import History from "./pages/History";
-import Recipe from "./pages/Recipe";
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/dietary" element={<Dietary />} />
-        <Route exact path="/intolerance" element={<Intolerance />} />
-        <Route exact path="/country" element={<Country />} />
-        <Route exact path="/history" element={<History />} />
-        <Route exact path="/recipe/:name/:id" element={<Recipe />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
